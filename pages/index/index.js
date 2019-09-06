57413e09a5ccc0dc7be62def9f1f5e1d5c4d6f7c
zhoususu <431185521@qq.com>
2019-09-07 00:33:31 +0800

    初始化

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,40 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+
+      imgUrls: [
+        'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+        'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+      ],
+      backgroundImages:"./images/background.jpg",
+      indicatorDots: true,
+      autoplay: false,
+      interval: 5000,
+      duration: 500,
+      serviceList: [{
+        title:"快递代拿",
+        icon:'./images/icon1.png'
+      }, 
+      {
+        title: "快递代拿",
+        icon: './images/icon2.png'
+      }, 
+      {
+        title: "快递代拿",
+        icon: './images/icon3.png'
+        }, {
+          title: "快递代拿",
+        icon: './images/icon3.png'
+        },
+        {
+          title: "快递代拿",
+          icon: './images/shield.png'
+        },
+        {
+          title: "快递代拿",
+          icon: './images/shield.png'
+        }],
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  
 })
